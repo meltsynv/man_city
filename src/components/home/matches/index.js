a5cdf808939534371461e3714dd8a1f573e9795d
meltsynv <meltynv@yahoo.com>
2019-10-09 16:58:22 +0200

    session one

--- src/components/home/matches/index.js
+++ src/components/home/matches/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import Tag from '../../ui/miscs'
 
 const MatchesHome = () => {
     return (
         <div className='home_matches_wrapper'>
             <div className='container'>
-                TAG
+                <Tag
+                    bck='#0e1731'
+                    size='50px'
+                    color='#ffffff'
+                >
+                    Matches
+                </Tag>
 
                 BLOCK
 
-                TAG
+                 <Tag
+                    bck='#ffffff'
+                    size='22px'
+                    color='#0e1731'
+                    link={true}
+                    linkTo='the_team'
+                >
+                   See more matches
+                </Tag>
             </div>
         </div>
     );
